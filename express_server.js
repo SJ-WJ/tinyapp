036f30ff22a61f40bbd6fa3ec150a8e85bc693f3
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-16 22:26:25 +0000

    Made correction with res.redirect in the app.post /urls and removed the colon

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs"); // Telling Express to use EJS as its templating engine
 
@@ -19,11 +23,15 @@ function generateRandomString() { //generating an alpha-numeric string
   return ans
 }
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.post("/login", (req,res) =>{
+  const userName = req.body.username
+  console.log(req.body);
+  res.cookie('userId', userName);
+  res.redirect("/urls");
+})
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls");
 });
 
 app.get("/urls/new", (req, res) => {
@@ -32,13 +40,15 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   let id = generateRandomString();
-  //add key:value pair to object
-  urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls/:${[id]}`);
+  const longURL = req.body.longURL
+  console.log(longURL);
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${[id]}`);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  // let userId = 
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -48,9 +58,20 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+
+
+app.post("/urls/:shortURL", (req,res) => {
+  const shortURL = req.params.shortURL
+  const longURL = req.body.longURL
+  console.log(longURL); //check for longURL
+  urlDatabase[shortURL] = longURL
+  res.redirect("/urls")
+})
+
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.shortURL
