a34369e1062613c450bd21deb2d2ad6acf3cfcc9
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-18 19:12:09 +0000

    Created helper function to check if an email already exists and created registration errors

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(cookieParser());
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs"); // Telling Express to use EJS as its templating engine
 
@@ -15,28 +15,41 @@ const urlDatabase = {
 
 const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 }
 
+// Helper Functions
 function generateRandomString() { //generating an alpha-numeric string
   const list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let ans = "";
   for (let i = 0; i < 6; i++) {
     let result = Math.floor(Math.random() * list.length);
     ans += list.charAt(result);
-  } 
+  }
   return ans
 }
 
-app.post("/login", (req,res) =>{
+// Helper Function: Checking if email exists
+function ifEmailExists(users, email) {
+  for (let user in users) {
+    if (users[user].email === email) {
+      return true;
+    }
+  } 
+  return false;
+};
+
+// Routes are down below
+
+app.post("/login", (req, res) => {
   const userName = req.body.username
   res.cookie('username', userName);
   res.redirect("/urls");
@@ -52,7 +65,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {user: users[req.cookies["user_id"]]}; 
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
@@ -65,17 +78,17 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls", (req, res) => {
   // let userId = 
-  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]]};
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
   res.render("urls_show", templateVars);
 });
 
 // Edit long URL route
-app.post("/urls/:shortURL", (req,res) => {
+app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   const longURL = req.body.updatedlongURL
   urlDatabase[shortURL] = longURL
@@ -91,21 +104,30 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 // Routing registration page to /register
 app.get("/register", (req, res) => {
-  const templateVars = {user: users[req.cookies["user_id"]]}
+  const templateVars = { user: users[req.cookies["user_id"]] }
   res.render("registration_page", templateVars)
 })
 
-app.post("/register", (req,res) => {
-const email = req.body.email
-const password = req.body.password
-const id = generateRandomString() 
-// creating the object
-users[id] = {id:id, email:email, password:password}
-
-res.cookie('user_id', id);
-res.redirect("/urls");
+app.post("/register", (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const id = generateRandomString()
+  
+  if (email === "" || password === "") {
+    return res.status(400).send("Email or Password is empty")
+  } 
+  
+  const emailExists = ifEmailExists(users, email)
+  if (emailExists === true) {
+    return res.status(400).send("An account with this email already exists")
+  }
+  users[id] = { id: id, email: email, password: password }
+  
+  res.cookie('user_id', id);
+  res.redirect("/urls");
 })
 
+// Message to let us know there the connection is working
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
